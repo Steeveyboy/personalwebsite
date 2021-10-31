cc90eae10750d394542cdaccdf0c60428282cb3b
Steeveyboy <jonathan.qsteeves@gmail.com>
2021-10-31 19:24:31 -0400

    books

--- my-app/src/components/books.js
+++ my-app/src/components/books.js
@@ -8,15 +8,21 @@ function books(){
             <div class="bookChoice">
             
                 <img src="images/theBomberMafia.jpg" alt=""></img>
-                <p>This is a description but the description will be smaller than the book itself. The bomber mafia chronicals the story of general hansell and the quest to bring strategic bombing into the mode.</p>
+                <div class="bookDescription">
+                    <p><i>The Bomber Mafia</i> is a story of innovation in the face of chaos. Taking place during the second world war, <i>The Bomber Mafia</i> attempts to chronicle the attempt of military thinkers to bring precision bombing to the forefront of war.</p>
+                </div>
             </div>
             <div class="bookChoice">
                 <img src="images/elonMuskBook.jpg" alt=""></img>
-                <p>This is a description but the description will be smaller than the book itself. The bomber mafia chronicals the story of general hansell and the quest to bring strategic bombing into the mode.</p>
+                <div class="bookDescription">
+                    <p>This authorized biography tells the story of Elon Musk. While often examining the string of failures that lead to his greatest successes, this book provides great insight into the motivations behind each of Musk's ventures.</p>
+                </div>
             </div>
             <div class="bookChoice">
                 <img src="images/measureWhatMatters.jpg" alt=""></img>
-                <p>This is a description but the description will be smaller than the book itself. The bomber mafia chronicals the story of general hansell and the quest to bring strategic bombing into the mode.</p>
+                <div class="bookDescription">
+                    <p><i>Measure what matters is in essence</i>, a book about how to set impossible goals, and somehow achieve them. Each chapter is a unique story about how prominent firms achieve their impossible, using a goal setting method that Doerr refers to as OKR's.</p>
+                </div>
             </div>
         </div>
     );
