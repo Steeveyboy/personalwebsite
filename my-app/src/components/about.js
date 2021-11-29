d05ae519dbc6602430fd3d4abd79b117d8813c9e
Steeveyboy <jonathan.qsteeves@gmail.com>
2021-11-29 16:22:26 -0500

    updates

--- my-app/src/components/about.js
+++ my-app/src/components/about.js
@@ -10,9 +10,9 @@ function about(){
             <img id="headshot" src="./images/mecircle.png" alt=""></img>
             <div class="aboutText">
                 <p>I am a third year student Carleton University taking Computer Sceince with a minor in Economics. 
-                I use several programming languages including Java, C/C++, Javascript, and Python. 
-                I am currently learning tools such as <b>Scikit-Learn</b> and practicing <b>Machine Learning </b> techniques so that I can improve at my job as a Data Analyst at Shared Services Canada.
-                I outside of work I enjoy reading, sailing, playing frisbee, and soccer. I am currently looking for work as per my Co-op Program, and am interested in any opporunities that involve Systems Engineering or Data Science.</p>
+                I use several programming languages including Java, C/C++, Javascript, and Python. I currently work as a student Software Developer at Shared Services Canada.
+                Right now I am learning tools such as <b>Scikit-Learn</b> and practicing <b>Machine Learning </b> techniques.
+                Outside of work I enjoy reading, sailing, playing frisbee, and soccer. I am currently looking for summer 2022 work opporunities, and am interested in anything that involves Systems Engineering or Data Science.</p>
             </div>
             <a href="https://github.com/Steeveyboy?tab=repositories">Github</a> 
             <a href="https://www.linkedin.com/in/jonathon-s-763ab211a/"> LinkedIn</a> 
