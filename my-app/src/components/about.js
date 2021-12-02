f34b65066093328c78d54fb53da71842759d91e7
Steeveyboy <jonathan.qsteeves@gmail.com>
2021-12-02 15:53:46 -0500

    typo

--- my-app/src/components/about.js
+++ my-app/src/components/about.js
@@ -9,7 +9,7 @@ function about(){
             <h2><u>About me</u></h2>
             <img id="headshot" src="./images/mecircle.png" alt=""></img>
             <div class="aboutText">
-                <p>I am a third year student Carleton University taking Computer Sceince with a minor in Economics. 
+                <p>I am a third year student Carleton University taking Computer Science with a minor in Economics. 
                 I use several programming languages including Java, C/C++, Javascript, and Python. I currently work as a student Software Developer at Shared Services Canada.
                 Right now I am learning tools such as <b>Scikit-Learn</b> and practicing <b>Machine Learning </b> techniques.
                 Outside of work I enjoy reading, sailing, playing frisbee, and soccer. I am currently looking for summer 2022 work opporunities, and am interested in anything that involves Systems Engineering or Data Science.</p>
