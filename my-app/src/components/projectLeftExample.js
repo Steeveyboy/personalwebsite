ab27bdf4636468630db2a7d767c761429e02b38b
Steeveyboy <jonathan.qsteeves@gmail.com>
2022-01-19 13:21:58 -0500

    Typos

--- my-app/src/components/projectLeftExample.js
+++ my-app/src/components/projectLeftExample.js
@@ -11,7 +11,7 @@ function projectExample({imagePath, title, text}){
                     where the user can contribute to the games evolution or destruction,
                     through setting the initial state.
                     In my version Conways Game of life, the player is allowed to alter the
-                    state of the cells at any point of the game abd alter the course of the
+                    state of the cells at any point of the game and alter the course of the
                     simulation. The population of the simulation is also tracked throughout the
                     game and graphed using chart.js, if iterested you can play the game 
                     <a href="https://steeveyboy.github.io/ConwaysChartOfLife"> <b>here.</b></a>
