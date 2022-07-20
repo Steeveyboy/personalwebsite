5fae5c02bb8545f43743c11e6a7781deeb32bc65
Steeveyboy <jonathan.qsteeves@gmail.com>
2022-07-20 12:29:20 -0400

    Update about.js

--- my-app/src/components/about.js
+++ my-app/src/components/about.js
@@ -12,14 +12,13 @@ function about(){
                 <p>I am a fourth year student Carleton University studying Computer Science with a minor in Economics. 
                 I use several programming languages including C++, Javascript, and Python, and enjoy making robots to automate monotonous tasks. 
                 I currently work as a Data Scientist at the Department of Finance Canada.
-                Right now I am learning tools such as <b>Scikit-Learn</b> and practicing <b>Machine Learning </b> techniques.
+                Right now I am learning tools such as <b>Scikit-Learn</b> and practicing <b>Machine Learning </b> techniques with a focus on natural language processing.
                 Outside of work I enjoy reading, sailing, playing frisbee, and karate. I am currently accepting clients and am open for contract work.</p>
             </div>
             <a href="https://github.com/Steeveyboy?tab=repositories">Github</a> 
             <a href="https://www.linkedin.com/in/jonathon-s-763ab211a/"> LinkedIn</a> 
             <a href="./images/JonathonSteeves.pdf"> Resume</a>
         </div>
-        
     );
 }
 
